98096c1fc52d5c1e0fe21910ade2487bb7a0e3cb
ShadYBhai <ashishnick.private@gmail.com>
2024-12-07 11:22:08 +0530

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import "./App.css";
 import BookList from "./components/BookList";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App: React.FC = () => (
   <div>
-    <BookList />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<BookList />} />
+      </Routes>
+    </Router>
   </div>
 );
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"; // If you're using React Router
+import "./navbar.css";
+
+const Navbar: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="navbar">
+      <div className="logo" onClick={() => navigate("/")}>
+        BookManager
+      </div>
+      <div className="nav-links">
+        <a href="#" onClick={() => navigate("/")}>
+          Home
+        </a>
+        <a href="#" onClick={() => navigate("/add")}>
+          Add Book
+        </a>
+        <a href="#" onClick={() => navigate("/about")}>
+          About
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
